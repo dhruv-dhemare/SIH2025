514b56a5ba752519aac84baa2ea03955df6aa14d
dhruv-dhemare <dhruvdhemare@gmail.com>
2025-09-10 20:23:40 +0530

    Basic backend structure

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const db =require('./db');
 
 const app = express();
 app.use(cors()); // allow frontend requests
 
 // Load JSON data (must be pure JSON array, NOT JS variable)
 const alumniData = JSON.parse(fs.readFileSync("./alumni_geocoded.json", "utf8"));
+const userRoutes = require('./routes/userRoutes');
 
 app.get("/api/alumni", (req, res) => {
   res.json(alumniData);
 });
+
+app.use('/user', userRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
